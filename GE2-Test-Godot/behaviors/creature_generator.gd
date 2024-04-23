ec95552ce1b94a0ed6ccb0373d571d0f23c6bf98
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2024-04-23 09:49:31 +0100

    Draw cube based on length

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -12,14 +12,20 @@ var base_size: float = 1
 @export
 var multiplier: float = 5
 
+var cube_positions = []
+
 func _process(delta):
-	DebugDraw3D.draw_box(Vector3(0,0,0), Quaternion(Vector3.RIGHT, 0), Vector3(base_size, base_size, base_size), Color.WHITE)
+	for cube in cube_positions:
+		DebugDraw3D.draw_box(cube, Quaternion(Vector3.RIGHT, 0), Vector3(base_size, base_size, base_size), Color.WHITE)
 	pass		
 
 func _ready():
 	if not Engine.is_editor_hint():		
 		pass
+	create_cube_locations()
 		
 
-
+func create_cube_locations():
+	for i in range(length):
+		cube_positions.append(Vector3(1*i, 0, 0))
 
