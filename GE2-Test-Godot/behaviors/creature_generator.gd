9f639e60f4a3694a0bcb67d78cb1914decfe8663
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2024-04-23 10:16:55 +0100

    Started Alignning the cubes

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -12,12 +12,21 @@ var base_size: float = 1
 @export
 var multiplier: float = 5
 
-var cube_positions = []
+var cube_positions: Array[Vector3] = []
 var cube_sizes = []
 
 func _process(delta):
 	for i in range(length):
-		DebugDraw3D.draw_box(cube_positions[i], Quaternion(Vector3.RIGHT, 0), cube_sizes[i], Color.WHITE)
+		var next_pos = cube_positions[i]
+		# Head Logic
+		if i == 0:
+			pass
+		else:
+			var prev_size = cube_sizes[i-1]
+			var prev_pos = cube_positions[i-1]
+			next_pos.x = prev_size.x + prev_pos.x
+			
+		DebugDraw3D.draw_box(next_pos, Quaternion(Vector3.RIGHT, 0), cube_sizes[i], Color.WHITE)
 		
 
 func _ready():
@@ -29,9 +38,10 @@ func _ready():
 func create_cube_locations():
 	for i in range(length):
 		# Need to convert the length to angle
-		var new_size = sin((frequency*i)+start_angle)*base_size
+		var angle = sin((frequency*i)+start_angle)
+		var new_size = remap(angle, 0, TAU, base_size, multiplier)
 		#var new_size = clamp(, 0, base_size)
 		print(new_size)
-		cube_positions.append(Vector3(1*i, 0, 0))
+		cube_positions.append(Vector3(new_size*i, 0, -(new_size/2)))
 		cube_sizes.append(Vector3(new_size, new_size, new_size))
 
