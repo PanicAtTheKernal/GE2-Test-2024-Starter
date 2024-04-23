fd5007c80604f079702c30e37bd4a5027ec311d9
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2024-04-23 09:41:06 +0100

    First cube

--- GE2-Test-Godot/behaviors/creature_generator.gd
+++ GE2-Test-Godot/behaviors/creature_generator.gd
@@ -1,14 +1,25 @@
 @tool
 extends Node3D
 
+@export
+var length: int = 20
+@export
+var start_angle: float = 0
+@export
+var frequency: float = 1
+@export
+var base_size: float = 1
+@export
+var multiplier: float = 5
 
 func _process(delta):
+	DebugDraw3D.draw_box(Vector3(0,0,0), Quaternion(Vector3.RIGHT, 0), Vector3(base_size, base_size, base_size), Color.WHITE)
 	pass		
 
 func _ready():
 	if not Engine.is_editor_hint():		
 		pass
-	
+		
 
 
 
